14fee07d707f954fadc13d40e9a45625a847974d
najira <najira_daniela@hotmail.com>
2024-05-20 13:33:27 -0400

    axios

--- index.js
+++ index.js
@@ -1,10 +1,22 @@
 import express from 'express'
+import axios from 'axios'
 
 const app = express()
 
 app.get('/', (req, res) => {
     res.send('titulo pagina princpal')
 })
+
+const randomInfo = async () => {
+    try {
+        const response = await axios.get('https://randomuser.me/api/')
+        console.log(response.data)
+    } catch (error) { console.log(error) }
+
+}
+randomInfo()
+
+
 
 const PORT = process.env.PORT || 5000
 app.listen(PORT, () => {
