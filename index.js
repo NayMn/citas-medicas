23a4ef146c631bfe87f78dd1105b9d995c3d9fdc
najira <najira_daniela@hotmail.com>
2024-05-20 13:59:05 -0400

    obtener usuario

--- index.js
+++ index.js
@@ -5,20 +5,25 @@ import moment from 'moment'
 import _ from 'lodash'
 import chalk from 'chalk'
 
-
-
 const app = express()
 
-const randomInfo = async () => {
+
+// obtener un usuario cualquiera: 
+const randomUsuario = async () => {
     try {
         const response = await axios.get('https://randomuser.me/api/')
-        console.log(response.data)
+        const dataUsuario = response.data.results[0]
+        return {
+            id: uuidv4(),
+            name: `${dataUsuario.name}`,
+            gender: dataUsuario.gender,
+            timestamp: moment().format('YYYY-MM-DD HH:mm:ss')
+        }
     } catch (error) { console.log(error) }
-
 }
-randomInfo()
+randomUsuario()
 
-
+console.log(randomUsuario())
 
 const PORT = process.env.PORT || 5000
 app.listen(PORT, () => {
