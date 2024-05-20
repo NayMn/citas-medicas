a9fcbd487aae3a2ccb8bcf539f196226cfc6934d
najira <najira_daniela@hotmail.com>
2024-05-20 15:17:15 -0400

    ruta usuarios y arreglos

--- index.js
+++ index.js
@@ -5,41 +5,47 @@ import moment from 'moment'
 import _ from 'lodash'
 import chalk from 'chalk'
 
-const app = express()
+const app = express();
 
-const usuarios = []
+const usuarios = [];
 
-// obtener un usuario cualquiera: 
+
 const randomUsuario = async () => {
     try {
-        const response = await axios.get('https://randomuser.me/api/')
-        const dataUsuario = response.data.results[0]
+        const response = await axios.get('https://randomuser.me/api/');
+        const dataUsuario = response.data.results[0];
         return {
             id: uuidv4(),
-            name: `${dataUsuario.name}`,
+            name: `${dataUsuario.name.first} ${dataUsuario.name.last}`,
             email: dataUsuario.email,
             gender: dataUsuario.gender,
             timestamp: moment().format('YYYY-MM-DD HH:mm:ss')
-        }
-    } catch (error) { console.log(error) }
-}
-randomUsuario()
+        };
+    } catch (error) {
+        console.error('Error fetching user:', error);
+        throw error;
+    }
+};
 
-// registrar un nuevo usuario: 
 
 app.get('/registrar', async (req, res) => {
     try {
         const usuarioNuevo = await randomUsuario();
-        usuarios.push(usuarioNuevo)
-        res.json(usuarioNuevo)
+        usuarios.push(usuarioNuevo);
+        res.json(usuarioNuevo);
     } catch (error) {
-        res.status(500).json({ error: 'error no se puede registrar el usuario' })
+        res.status(500).json({ error: 'Error no se puede registrar el usuario' });
     }
-})
+});
 
 
+app.get('/usuarios', (req, res) => {
+    const usuariosMF = _.groupBy(usuarios, 'gender');
+    console.log(chalk.bgWhite.blue(JSON.stringify(usuariosMF, null, 2)));
+    res.json(usuariosMF);
+});
 
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
-    console.log(`servidor escuchando en puerto ${PORT}`)
-})
+    console.log(`Servidor escuchando en puerto ${PORT}`);
+});
